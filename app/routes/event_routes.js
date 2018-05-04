7c9952b9315445468a28e0057ed1460cb8092aa6
Ilja <iljapanic@gmail.com>
2018-05-04 16:17:53 +0100

    return a redacted event object

--- app/routes/event_routes.js
+++ app/routes/event_routes.js
@@ -8,6 +8,7 @@ module.exports = function (app, db) {
 
   // today's events
   app.get('/london/events/today', (req, res) => {
+
     const query = {
       // start_date: '2018-05-04'
       start_datetime: {
@@ -16,7 +17,24 @@ module.exports = function (app, db) {
       }
     };
 
-    db.collection('events').find(query).toArray(function(err, results) {
+    const sort = {
+      event_popularity: -1
+    }
+
+    const projection = {
+      event_name: 1,
+      event_url: 1,
+      event_type: 1,
+      event_popularity: 1,
+      start_datetime: 1,
+      location: 1,
+      venue_id: 1,
+      venue_name: 1,
+      venue_location: 1,
+      artists: 1
+    };
+
+    db.collection('events').find(query).sort(sort).project(projection).toArray(function(err, results) {
       if (err) {
         res.send({ 'error': 'An error has occurred' });
       } else {
